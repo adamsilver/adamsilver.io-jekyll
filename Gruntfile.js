ee423276bd87414e0ffc27ce247e24224d2c1402
Adam Silver <adambsilver@gmail.com>
2016-07-21 18:34:38 +0100

    publish: Merge branch 'master' of https://github.com/adamsilver/adamsilver.io
    generated from commit 828d11cab6635089f41a7bc081bb94b24a634a5a

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		clean: {
+			css: [
+				'_site/assets/css/*.css'
+			]
+		},
+		cssmin: {
+			options: {
+				report: 'gzip'
+			}
+		},
+		filerev: {
+			dist: {
+                src: [
+                    '_site/assets/css/*.css'
+                ]
+            }
+		},
+		useminPrepare: {
+			html: '_site/signup/index.html',
+			options: {
+				dest: '_site/',
+				root: '_site/'
+			}
+		},
+		usemin: {
+			html: '_site/**/*.html',
+			options: {
+				assetsDirs: ['_site']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-filerev');
+	grunt.loadNpmTasks('grunt-usemin');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+
+	grunt.registerTask('log', function() {
+		console.log(grunt.filerev.summary);
+	});
+
+	grunt.registerTask('default', [
+		'clean:css',
+		'useminPrepare',
+		'concat:generated',
+		'cssmin:generated',
+		'filerev:dist',
+		'usemin'
+	]);
+
+};
